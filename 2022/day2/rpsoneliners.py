851a2d0804edb488db6897e6646e4dd789e2fa03
Robbie Fishel <robbie.fishel6@gmail.com>
2022-12-04 11:30:34 -0500

    Added shell script to run all existing 2022 solution code at once

--- 2022/day2/rpsoneliners.py
+++
@@ -1,4 +0,0 @@
-print("Star 1 Solution:", 
-    sum(map(lambda x: ((ord(x[2])-87) + ((((ord(x[2])-87)-(ord(x[0])-65))%3)*3)), open("rockpaperscissors.txt").readlines())))
-print("Star 2 Solution:", 
-    sum(map(lambda x : (((ord(x[2])-88)*3) + (((ord(x[2])-88)+(ord(x[0])-63))%3)+1), open("rockpaperscissors.txt").readlines())))
